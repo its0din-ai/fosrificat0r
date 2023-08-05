0076d4b662fe31aaa25d6fdf0579c94b89f27351
W. SYAFIUDDIN <encrypt0r-x@protonmail.com>
2023-08-05 17:06:49 +0700

    fungsi dasar verif

---
+++ mailer.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import { SMTPClient } from 'emailjs';
+
+const client = new SMTPClient({
+	user: 'encrypt0r.x86@gmail.com',
+	password: dotenv.config().parsed.APP_PASSWORD,
+	host: 'smtp.gmail.com',
+	ssl: true,
+});
+
+function sendMail(addr, code){
+    client.send(
+        {
+            text: 'Verification Code is: ' + code,
+            from: 'encrypt0r <encrypt0r.x86@gmail.com>',
+            to: `someone <${addr}>`,
+            subject: 'fosrivicat0r',
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
+}
+
+export { sendMail };
